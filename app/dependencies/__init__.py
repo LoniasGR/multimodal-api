2574544f3fa56018b63e88f3a01068c6e130b17b
Leonidas Avdelas <avdelasleonidas@gmail.com>
2026-01-30 14:37:35 +0200

    Fix sqllite path
    Signed-off-by: Leonidas Avdelas <avdelasleonidas@gmail.com>

--- app/dependencies/__init__.py
+++ app/dependencies/__init__.py
@@ -6,7 +6,7 @@ from sqlmodel import Session, SQLModel, create_engine
 
 from ..config import SQLITE_FILE_NAME
 
-sqlite_url = f"sqlite://../{SQLITE_FILE_NAME}"
+sqlite_url = f"sqlite:///..{SQLITE_FILE_NAME}"
 
 connect_args = {"check_same_thread": False}
 engine = create_engine(sqlite_url, connect_args=connect_args)
